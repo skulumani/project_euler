efb82cd074a6581fcc5e03abd02bc32e2ddcd933
Shankar Kulumani <shanks.k@gmail.com>
2017-02-20 18:55:09 -0500

    Add another prime seive

--- euler_python.py
+++ euler_python.py
@@ -79,6 +79,14 @@ def primesfrom2to(num):
             sieve[k*(k-2*(i&1)+4)//3::2*k] = False
     return np.r_[2,3,((3*np.nonzero(sieve)[0][1:]+1)|1)]
 
+def primesfrom3to(n):
+    """ Returns a array of primes, 3 <= p < n """
+    sieve = numpy.ones(n/2, dtype=numpy.bool)
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i/2]:
+            sieve[i*i/2::i] = False
+    return 2*numpy.nonzero(sieve)[0][1::]+1
+    
 def p3(num):
     """Problem 3
     The prime factors of 13195 are 5, 7, 13 and 29.
